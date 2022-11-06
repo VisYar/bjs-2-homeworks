4342b3eebb9588c5a5a41c2df5eab10814113f2d
Tatyana Dmitrieva <visyar76@gmail.com>
2022-11-06 19:25:19 +0300

    Added function solveEquation task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+	let arr;
+	let d = Math.pow(b, 2) - 4 * a * c;
+	if (d < 0) {
+		arr = [];
+	} else if (d === 0) {
+		arr = [-b / (2 * a)]
+	} else if (d > 0) {
+		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+  }
+	return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
