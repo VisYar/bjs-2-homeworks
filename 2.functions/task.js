e5b0e6a3c4bc17000c87254ffd6ddfa99bcb7e30
Tatyana Dmitrieva <visyar76@gmail.com>
2022-11-07 09:11:28 +0300

    function getArrayParams task1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,21 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
-}
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		sum += arr[i];
+	}
+		avg = Number((sum / arr.length).toFixed(2));
+		return { min: min, max: max, avg: avg };
+	}
 
 // Задание 2
 function worker(arr) {
