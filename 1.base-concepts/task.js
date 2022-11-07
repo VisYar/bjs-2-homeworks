92a0e8299c15d58ccb65e6e6868782179e0d0086
Tatyana Dmitrieva <visyar76@gmail.com>
2022-11-07 11:00:47 +0300

    Changed the names of variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,13 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  let d = Math.pow(b, 2) - 4 * a * c;
-  if (d < 0) {
+	let arr;
+	const discriminan = Math.pow(b, 2) - 4 * a * c;
+  if (discriminan < 0) {
     arr = [];
-  } else if (d === 0) {
+  } else if (discriminan === 0) {
       arr = [-b / (2 * a)]
-    } else if (d > 0) {
-        arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+    } else if (discriminan > 0) {
+        arr = [(-b + Math.sqrt(discriminan)) / (2 * a), (-b - Math.sqrt(discriminan)) / (2 * a)]
       }
   return arr;
 }
@@ -28,10 +28,10 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   amount = Number(amount);
   date = Number(date);
   let nowDate = new Date;
-  let S = amount - contribution;
-  let n = Math.trunc(Math.abs(date - nowDate) / (1000 * 60 * 60 * 24 * 30));
-  let P = percent / 12 / 100;
-  let payment = S * (P + (P / (Math.pow(1 + P, n) - 1)));
-  totalAmount = Number((payment * n).toFixed(2));
+  let loanBody = amount - contribution;
+  let numberOfMonths = Math.trunc(Math.abs(date - nowDate) / (1000 * 60 * 60 * 24 * 30));
+  let twelfthPartOfInterestRate = percent / 12 / 100;
+  let payment = loanBody * (twelfthPartOfInterestRate + (twelfthPartOfInterestRate / (Math.pow(1 + twelfthPartOfInterestRate, numberOfMonths) - 1)));
+  totalAmount = Number((payment * numberOfMonths).toFixed(2));
   return totalAmount;
 }
