12f94322b6333774eceb158ede5f31c05f0f2a15
Tatyana Dmitrieva <visyar76@gmail.com>
2022-11-08 11:36:50 +0300

    Added function task 1-2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+	let result;
+	if (arr1.length !== arr2.length) {
+		return false;
+	} else {
+		result = arr1.every((e, index) => e === arr2[index]);
+	}
+	return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+	let resultArr;
+	resultArr = arr.filter(e => e > 0).filter(e => e % 3 === 0).map(e => e * 10);
+	return resultArr; // array
 }
