f5dd56e1a868a42707e46d780ab3fc243dc5fdfb
Tatyana Dmitrieva <visyar76@gmail.com>
2022-11-09 13:28:08 +0300

    Task 1 Encapsulation of the student

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+	this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+	if(this.marks === undefined) {
+		this.marks = [mark];
+	} else {
+		this.marks.push(mark);
+	}
+}
+
+Student.prototype.addMarks = function (...arr) {
+	if (this.marks === undefined) {
+		this.marks = [...arr];
+	} else {
+		this.marks.push(...arr)
+	};
+ }
+
+Student.prototype.getAverage = function () {
+	let sum = 0;
+	this.marks.map(item => sum += item);
+	return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
+}
